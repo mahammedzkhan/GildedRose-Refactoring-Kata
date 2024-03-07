d8878a0c68d414813adb9f6979ae537ff330ab01
Zishan.Khan <zishan.khan@bluecorner.be>
2024-03-07 19:38:37 +0100

    test: add test for degrading by 1 in case sellIn hasn't passed

--- TypeScript/test/vitest/gilded-rose.spec.ts
+++ TypeScript/test/vitest/gilded-rose.spec.ts
@@ -4,6 +4,14 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
   });
+
+  it("should degrade quality by 1 because sellIn hasn't passed", () => {
+    const gildedRose = new GildedRose([new Item('Average Quality Cheese', 2, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(9);
+    expect(items[0].sellIn).toBe(1);
+  });
+
 });
