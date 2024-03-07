820e0481c7a1d41ce4081a4474d6ed3da990ee44
Zishan.Khan <zishan.khan@bluecorner.be>
2024-03-07 19:49:54 +0100

    test: add test for dropping quality to 0 for backstage passes after concert

--- TypeScript/test/vitest/gilded-rose.spec.ts
+++ TypeScript/test/vitest/gilded-rose.spec.ts
@@ -63,4 +63,11 @@ describe('Gilded Rose', () => {
     expect(items[0].sellIn).toBe(4);
   });
 
+  it('should drop quality to 0 for backstage passes after the concert', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+    expect(items[0].sellIn).toBe(-1);
+  });
+
 });
